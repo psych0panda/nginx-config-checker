000a9c4ff577fa6fcb66be81f57046e3bd14917b
ipyatigorets <i.pyatigorets@gmail.com>
2025-01-17 16:17:24 +0000

    Добавлено использование Docker для проверки конфигурации NGINX, обновлен компонент проверки конфигурации, добавлен Dockerfile и настройки окружения.

--- backend/app/api/v1/endpoints/config_checker.py
+++ backend/app/api/v1/endpoints/config_checker.py
@@ -1,48 +1,39 @@
-import re
-from fastapi import APIRouter, UploadFile, File, HTTPException
-import subprocess
 import os
+import docker
+from fastapi import APIRouter, UploadFile, File, HTTPException
 from dotenv import load_dotenv
 
 router = APIRouter()
+client = docker.from_env()
+
 
 @router.post("/check-config/")
 async def check_nginx_config(file: UploadFile = File(...)):
+    load_dotenv()
+    base_dir = os.getenv('BASE_DIR', '/tmp')
+    temp_file_path = f"{base_dir}/{file.filename}"
+    with open(temp_file_path, "wb") as temp_file:
+        content: bytes = await file.read()
+        temp_file.write(content)
+    
     try:
-        load_dotenv()
-        base_dir = os.getenv('BASE_DIR', '/tmp')
-        # Сохраняем загруженный файл во временное хранилище
-        temp_file_path = f"{base_dir}/{file.filename}"
-        print(temp_file_path)
-        with open(temp_file_path, "wb") as temp_file:
-            content = await file.read()
-            temp_file.write(content)
-        if not os.path.exists(temp_file_path):
-            raise HTTPException(status_code=404, detail="Ошибка сохранения файла.")        
-
-        # Проверяем структуру конфигурационного файла
-        with open(temp_file_path, "r") as temp_file:
-            config_content = temp_file.read()
-            if not re.search(r'events\s*{', config_content):
-                raise HTTPException(status_code=400, detail="Конфигурационный файл должен содержать блок 'events'.")
-            if not re.search(r'http\s*{', config_content):
-                raise HTTPException(status_code=400, detail="Конфигурационный файл должен содержать блок 'http'.")
-
-        # Создаем Docker контейнер для проверки конфигурации NGINX
-        docker_command = [
-            "docker", "run", "--rm",
-            "-v", f"{temp_file_path}:/etc/nginx/nginx.conf",
-            "nginx:latest", "nginx", "-t"
-        ]
-        result = subprocess.run(docker_command, capture_output=True, text=True)
-
-        # Удаляем временный файл
+        container = client.containers.run(
+            "nginx:latest",
+            detach=True,
+            command=f"nginx -t -c {temp_file_path}",
+            ports={"80/tcp": 8080},
+            remove=False
+        )
+        container.wait()
+        logs = container.logs()
+        container.remove()
         os.remove(temp_file_path)
-
-        if result.returncode == 0:
-            return {"status": "success", "message": "Конфигурация корректна."}
-        else:
-            raise HTTPException(status_code=400, detail=result.stderr)
-
+        return {"status": "success", "message": f"{logs.decode()}"}
+    except docker.errors.ContainerError as e:
+        raise HTTPException(status_code=400, detail=e.stderr.decode())
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        if os.path.exists(temp_file_path):
+            print("Удаление временного файла...")
+            os.remove(temp_file_path)
